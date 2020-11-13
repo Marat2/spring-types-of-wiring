e30b2394c4b4e98d3ea7ded9c3c4b7b21669f3a6
Your Name <you@example.com>
2020-11-13 17:32:52 +0500

    first spring-types-of-wiring

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,12 +1,20 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
 
+    Egg6 eggs;
+
+    @Autowired
+    public void setEggs(Egg6 eggs) {
+        this.eggs = eggs;
+    }
+
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо " + eggs.toString();
     }
 }
